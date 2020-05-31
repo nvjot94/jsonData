a9c5455c1bd189c5f3d0663c34122001d0a0824e
Navjot Singh <nvjot94@gmail.com>
2020-05-31 05:02:13 -0400

    Create projects.js

---
+++ projects.js
@@ -0,0 +1,16 @@
+const data = [
+	{
+		name: 'Project 1',
+		githubUrl: 'https://www.github.com/nvjot94',
+		liveUrl: 'https://www.github.com/nvjot94',
+		imgUrl:
+			'https://pocket-syndicated-images.s3.amazonaws.com/5e4428b7bfde1.jpg'
+	},
+	{
+		name: 'Project 2',
+		githubUrl: 'https://www.github.com/nvjot94',
+		liveUrl: 'https://www.github.com/nvjot94',
+		imgUrl:
+			'https://pocket-syndicated-images.s3.amazonaws.com/5e4428b7bfde1.jpg'
+	}
+];
